a395c605895a60eebef6489d73b1dedfc926c760
Gleb Kosteiko <gleb.kosteiko@gmail.com>
2018-11-22 16:33:14 +0300

    Create JwtAuthenticationEntryPointTest

---
+++ src/test/java/gk/defpub/restservice/security/JwtAuthenticationEntryPointTest.java
@@ -0,0 +1,53 @@
+package gk.defpub.restservice.security;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+/**
+ * JwtAuthenticationEntryPointTest class.
+ * <p>
+ * Date: Nov 22, 2018
+ * <p>
+ *
+ * @author Gleb Kosteiko
+ */
+@RunWith(SpringRunner.class)
+public class JwtAuthenticationEntryPointTest {
+    @Mock
+    private HttpServletRequest request;
+    @Mock
+    private HttpServletResponse response;
+    @Mock
+    private AuthenticationException authException;
+
+    @InjectMocks
+    private JwtAuthenticationEntryPoint entryPoint = new JwtAuthenticationEntryPoint();
+
+    @Before
+    public void setUp() {
+        initMocks(this);
+    }
+
+    @Test
+    public void testCommence() throws IOException {
+        entryPoint.commence(request, response, authException);
+        verify(response).sendError(eq(HttpServletResponse.SC_UNAUTHORIZED), anyString());
+        verifyNoMoreInteractions(response);
+    }
+}
