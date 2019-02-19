43787a1de64e817a9a1420e44845cdcc463b20f4
Gleb Kosteiko <gleb.kosteiko@gmail.com>
2019-02-19 18:10:47 +0300

    Remove regex for password validation

--- src/main/java/gk/defpub/restservice/util/Constants.java
+++ src/main/java/gk/defpub/restservice/util/Constants.java
@@ -9,7 +9,7 @@ package gk.defpub.restservice.util;
  * @author Gleb Kosteiko
  */
 public class Constants {
-    public static final long ACCESS_TOKEN_VALIDITY_SECONDS = 5*60*60;
+    public static final long ACCESS_TOKEN_VALIDITY_SECONDS = 5 * 60 * 60;
     public static final String SIGNING_KEY = "defPub0923Check";
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
